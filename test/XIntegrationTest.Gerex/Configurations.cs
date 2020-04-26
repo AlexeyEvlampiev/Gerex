2041112959253515c5f6de2e79e816a418f3092a
Alexey Evlampiev <alexey.evlampiev@gmail.com>
2020-04-26 17:42:35 +0200

    wip

---
+++ test/XIntegrationTest.Gerex/Configurations.cs
@@ -0,0 +1,66 @@
+﻿namespace Gerex
+{
+    using System;
+    using System.Threading.Tasks;
+    using Microsoft.Azure.ServiceBus.Management;
+    using Microsoft.Extensions.Configuration;
+
+    class Configurations
+    {
+        public const string TopicName = "b3e929ad-a52b-4074-833d-72056d02c61f";
+        public const string SubscriptionName = "shared";
+        public const string QueueName = "e16f65f6-9207-40fd-8779-ec61f422857a";
+
+        static readonly Lazy<Configurations> _instance = new Lazy<Configurations>(()=> new Configurations());
+
+        public static Configurations Instance => _instance.Value;
+
+        private Configurations()
+        {
+            var config =
+                new ConfigurationBuilder()
+                    .AddJsonFile("appsettings.json")
+                    .Build();
+            ConnectionString = config["ConnectionString"];
+            if (String.IsNullOrWhiteSpace(ConnectionString))
+                throw new InvalidOperationException($"Connection string value is missing");
+
+            var management = new ManagementClient(ConnectionString);
+            CreateTopicAsync(management).GetAwaiter().GetResult();
+            CreateQueueAsync(management).GetAwaiter().GetResult();
+
+        }
+
+        public string ConnectionString { get; }
+
+        private async Task CreateQueueAsync(ManagementClient management)
+        {
+            if (false == await management.QueueExistsAsync(QueueName))
+            {
+                await management.CreateQueueAsync(QueueName);
+            }
+        }
+
+       
+
+        private async Task CreateTopicAsync(ManagementClient management)
+        {
+            if (false == await management.TopicExistsAsync(TopicName))
+            {
+                await management.CreateTopicAsync(TopicName);
+            }
+
+            if (false == await management.SubscriptionExistsAsync(TopicName, SubscriptionName))
+            {
+                await management.CreateSubscriptionAsync(new SubscriptionDescription(TopicName, SubscriptionName)
+                {
+                    AutoDeleteOnIdle = TimeSpan.FromMinutes(20), 
+                    DefaultMessageTimeToLive = TimeSpan.FromMinutes(1),
+                    MaxDeliveryCount = 2
+                });
+            }
+        }
+
+
+    }
+}
--- test/XIntegrationTest.Gerex/ServiceBusConnection_ProcessMessages_Should.cs
+++ test/XIntegrationTest.Gerex/ServiceBusConnection_ProcessMessages_Should.cs
@@ -6,16 +6,18 @@ namespace Gerex
     using System.Threading.Tasks;
     using Microsoft.Azure.ServiceBus;
     using Xunit;
+    using static Configurations;
 
     public class ServiceBusConnection_ProcessMessages_Should
     {
         [Fact]
-        public async Task Test1()
+        public async Task Work()
         {
-            ServiceBusConnection connection = new ServiceBusConnection("");
+            var config = Configurations.Instance;
+            var connection = new ServiceBusConnection(config.ConnectionString);
             await connection
                 .ProcessMessages((message, ct) => Task.CompletedTask)
-                .FromSubscription("topic", "subscription", ReceiveMode.PeekLock, RetryPolicy.Default)
+                .FromSubscription(TopicName, SubscriptionName, ReceiveMode.PeekLock, RetryPolicy.Default)
                 .WithErrorHandler(args=> Task.CompletedTask);
         }
     }
